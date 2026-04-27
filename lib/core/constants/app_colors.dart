adf4f563cf4cc4986c3948a647e56856bb485af4
Shafiq <shafiq.rehman@nextgeni.com>
2026-04-27 15:24:21 +0500

    App images

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const kPrimaryColor = Color(0xFF3D3D33);
+  static const whiteColor = Color(0xFFFFFFFF);
+  static const blackColor = Color(0xFF000000);
+  static const redColor = Color(0xFFDD1212);
+  static const scaffoldColor = Color(0xFFFFFFFF);
+  static const dividerColor = Color(0xFFDADADA);
+  static const maroonColor = Color(0xFFA04838);
+  static const removeColor = Color(0xFFDA3737);
+  static const linearProgressBorderColor = Color(0xFFEDEDED);
+  static const transparentColor = Colors.transparent;
+  static const greyColor = Color(0xFF6E6E6E);
+  static const iconsGreyColor = Color(0xFFA9A9A9);
+  static const thumbBarGreyColor = Color(0xFFCCCCCC);
+  static const shimmerGreyColor = Color(0xFFEEEEEE);
+  static const shimmerHighlightColor = Color(0xFFE0E0E0);
+  static const greyBottomSheetThumbColor = Color(0xFFCCCCCC);
+  static const greyGridBoxColor = Color(0xFFDBDBDB);
+  static const hintColor = Color(0xFFB6B6B6);
+  static const sandColor = Color(0xFFE8E6DC);
+  static const myAccountBorderColor = Color(0xFFE2E2E2);
+  static const colorsOutlineColor = Color(0xFFEBEBEB);
+
+}
---
+++ lib/core/constants/app_images.dart
@@ -0,0 +1,6 @@
+class AppImages {
+  static final String iconPath = 'assets/icons/';
+  static final String imagePath = 'assets/images/';
+  static final String hubcoLogo = '${imagePath}hubco_splash.png';
+  static final String nawaTextIcon = '${iconPath}nawa_text_icon.svg';
+}
