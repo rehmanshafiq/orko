aaf01158b07fc38738c4c757f3a6b2d4aae055ed
shafiqrehmannextgeni <shafiq.rehman@nextgeni.com>
2026-06-01 09:52:33 +0500

    Client feedbacks

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.orko_hubco.mobile.orko_hubco"
+        applicationId = "com.orko_hubco.mobile.orko_hubco1"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
