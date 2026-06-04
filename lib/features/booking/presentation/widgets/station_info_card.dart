e3b73eda1a4b8fd3e2f5fdf5ec17fe526e4aa782
shafiqrehmannextgeni <shafiq.rehman@nextgeni.com>
2026-06-04 16:16:09 +0500

    Map icons updates changes done

--- lib/features/booking/presentation/widgets/station_info_card.dart
+++ lib/features/booking/presentation/widgets/station_info_card.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:orko_hubco/core/constants/app_colors.dart';
+import 'package:orko_hubco/core/constants/app_images.dart';
 import 'package:orko_hubco/core/constants/app_sizes.dart';
 import 'package:orko_hubco/core/utils/app_ui.dart';
 import 'package:orko_hubco/core/utils/widgets/app_text.dart';
+import 'package:orko_hubco/core/utils/widgets/image_view/app_image_view.dart';
 
 class StationInfoCard extends StatelessWidget {
   const StationInfoCard({
@@ -47,11 +49,23 @@ class StationInfoCard extends StatelessWidget {
           10.verticalSpace,
           Row(
             children: [
-              _PlugChipRow(ui: ui, label: 'CCS'),
+              _PlugChipRow(
+                ui: ui,
+                label: 'CCS',
+                assetPath: AppImages.icCss2,
+              ),
               12.horizontalSpace,
-              _PlugChipRow(ui: ui, label: 'CHAdeMO'),
+              _PlugChipRow(
+                ui: ui,
+                label: 'CHAdeMO',
+                assetPath: AppImages.icCss2,
+              ),
               12.horizontalSpace,
-              _PlugChipRow(ui: ui, label: 'Type 2'),
+              _PlugChipRow(
+                ui: ui,
+                label: 'Type 2',
+                assetPath: AppImages.icCss2,
+              ),
             ],
           ),
         ],
@@ -61,17 +75,27 @@ class StationInfoCard extends StatelessWidget {
 }
 
 class _PlugChipRow extends StatelessWidget {
-  const _PlugChipRow({required this.ui, required this.label});
+  const _PlugChipRow({
+    required this.ui,
+    required this.label,
+    required this.assetPath,
+  });
 
   final AppUiColors ui;
   final String label;
+  final String assetPath;
+
+  static const _iconSize = 28.0;
 
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisSize: MainAxisSize.min,
       children: [
-        Icon(Icons.ev_station_outlined, size: 16.sp, color: ui.textSecondary),
+        _PlugChipIcon(
+          assetPath: assetPath,
+          color: ui.textSecondary,
+        ),
         4.horizontalSpace,
         AppText(
           label,
@@ -80,6 +104,38 @@ class _PlugChipRow extends StatelessWidget {
           fontWeight: FontWeights.weight400,
         ),
       ],
+    );
+  }
+}
+
+class _PlugChipIcon extends StatelessWidget {
+  const _PlugChipIcon({
+    required this.assetPath,
+    required this.color,
+  });
+
+  final String assetPath;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    final size = _PlugChipRow._iconSize.sp;
+
+    if (assetPath.endsWith('.svg')) {
+      return AppSvgImageView(
+        appImagePath: assetPath,
+        width: size,
+        height: size,
+        fit: BoxFit.contain,
+        color: color,
+      );
+    }
+
+    return AppPngImageView(
+      appImagePath: assetPath,
+      width: size,
+      height: size,
+      fit: BoxFit.contain,
     );
   }
 }
