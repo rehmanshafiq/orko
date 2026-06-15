b0bc8e33c49426df38da48e40da4a8d1f25644e8
shafiqrehmannextgeni <shafiq.rehman@nextgeni.com>
2026-06-15 16:40:58 +0500

    Profile screen changes + start charge button removed + Firebase setup and remote config implemented

--- lib/features/remote_config/di/remote_config_injection.dart
+++ lib/features/remote_config/di/remote_config_injection.dart
@@ -1,25 +1,14 @@
 import 'package:orko_hubco/core/di/injection_container.dart';
-import 'package:orko_hubco/features/remote_config/data/datasources/remote/remote_config_datasource.dart';
-import 'package:orko_hubco/features/remote_config/data/repositories/remote_config_repository_impl.dart';
-import 'package:orko_hubco/features/remote_config/domain/repositories/remote_config_repository.dart';
-import 'package:orko_hubco/features/remote_config/domain/usecases/fetch_remote_config_usecase.dart';
-import 'package:orko_hubco/features/remote_config/presentation/cubit/remote_config_cubit.dart';
+import 'package:orko_hubco/features/remote_config/data/services/remote_config_service.dart';
+import 'package:orko_hubco/features/remote_config/presentation/bloc/remote_config_bloc.dart';
 
 /// Registers all Remote Config feature dependencies.
 void initRemoteConfigDependencies() {
-  // Data Source
-  sl.registerLazySingleton<RemoteConfigDataSource>(
-    () => RemoteConfigDataSourceImpl(),
+  // Service (singleton, holds in-memory config cache)
+  sl.registerLazySingleton<RemoteConfigService>(
+    () => RemoteConfigService.instance,
   );
 
-  // Repository
-  sl.registerLazySingleton<RemoteConfigRepository>(
-    () => RemoteConfigRepositoryImpl(dataSource: sl()),
-  );
-
-  // Use Case
-  sl.registerLazySingleton(() => FetchRemoteConfigUseCase(sl()));
-
-  // Cubit
-  sl.registerFactory(() => RemoteConfigCubit(fetchConfigUseCase: sl()));
+  // Bloc
+  sl.registerFactory(() => RemoteConfigBloc(service: sl()));
 }
